c2d7e5ed83f44aa4f18e712efffcc51a7ef62c0a
annalabi <lampiriioanna@gmail.com>
2023-12-11 21:45:37 +0200

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,27 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { MainComponent } from './main/main.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ProductsComponent } from './products/products.component';
+import { StoresComponent } from './stores/stores.component';
+import { ShoppingBasketComponent } from './shopping-basket/shopping-basket.component';
+import { FileNotFoundComponent } from './file-not-found/file-not-found.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet,
+            MainComponent,
+            NavbarComponent,
+            ProductsComponent,
+            StoresComponent,
+            ShoppingBasketComponent,
+            FileNotFoundComponent,
+            FooterComponent,
+            HeaderComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { MainComponent } from './main/main.component';
+import { StoresComponent } from './stores/stores.component';
+import { ProductsComponent } from './products/products.component';
+import { ShoppingBasketComponent } from './shopping-basket/shopping-basket.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FileNotFoundComponent } from './file-not-found/file-not-found.component';
+import { FooterComponent } from './footer/footer.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'main', component: MainComponent},
+    {path: 'stores', component: StoresComponent},
+    {path: 'products', component: ProductsComponent},
+    {path: 'shoppingBasket', component: ShoppingBasketComponent},
+    { path: '', redirectTo: 'home',  pathMatch: 'full' },
+    { path: '**', component: FileNotFoundComponent}
+];
