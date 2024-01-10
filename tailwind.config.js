5a89dbd8a56c1313e787d4bb5389c6c7146a2954
annalabi <lampiriioanna@gmail.com>
2024-01-10 21:34:31 +0200

    tailwind theme changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,13 +11,16 @@ module.exports = {
 
     colors: {
       'gray': "#8492a6",
-      'gray-dark': "#273444",
+      'gray-dark': "#111827",
       'gray-light': "#d3dce6",
-      'teal': '#143727',
+      'gray-100': '#f3f4f6',
+      'gray-700': '#374151',
+      'teal': '#042f2e',
       'white': '#fff',
       'offwhite': '#C8BEBE',
       'orange-50': '#fff7ed',
       'orange-800': '#9a3412',
+      'yellow-400': '#facc15',
       'yellow-600': '#ca8a04',
     },
 
@@ -26,6 +29,7 @@ module.exports = {
       spacing:{
         '128': '32rem',
         '144': '36rem',
+        '80': '20.5rem',
       },
 
       borderRadius:{
